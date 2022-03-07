b757536319ff78fe811672840e4c538936edf9c0
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-08 00:09:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from lib.client import Client
+from lib.hcaptcha import hCaptcha
 import config
 from discord import app_commands
 from sanic import text
 
 client = Client()
+hcaptcha = hCaptcha()
 
 @client.event
 async def on_ready():
@@ -12,5 +14,17 @@ async def on_ready():
 @client.route("/")
 async def main(request):
     return text("404 error")
+
+@client.web.get("/verify")
+async def verify(request):
+    pass
+
+@client.web.post("/verify")
+async def verify_check(request):
+    check = await hcaptcha.verify(request.form["h-captcha-response"])
+    if check:
+        pass
+    else:
+        pass
 
 client.run(config.token, config.ip, config.port)
