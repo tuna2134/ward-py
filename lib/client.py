9d917727b8eac659417b5f507e8243f109c747eb
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-08 00:06:26 +0900

    Update client.py

--- lib/client.py
+++ lib/client.py
@@ -8,11 +8,13 @@ class Client(Client):
         self.web = Sanic("web")
         self.route = self.web.route
         self.web.register_listener(self.process_start, "main_process_start")
+        self.tree = app_commands.CommandTree(self)
         
     async def process_start(self, app, loop):
         super().__init__(*self._args, **self._kwargs)
         loop.create_task(self.run(self.token))
         await self.wait_until_ready()
+        await self.tree.sync()
     
     def run(self, token, *args, **kwargs):
         self.token = token
